4ce83776afd6655ea4a57c5ac7302a6b7e1a87a0
Brandon <pzocikbrandon@gmail.com>
2024-09-02 16:41:53 -0300

    se cambio a createElement

--- src/main.js
+++ src/main.js
@@ -3,6 +3,27 @@ const $app = document.getElementById("app");
 const $createTaskForm = document.getElementById("create-task");
 const $tasksList = document.getElementById("tasksList");
 
+const renderTasks = (tasks) => {
+  const $container = document.createElement("div");
+  $container.classList.add("tasks-item");
+
+  const $info = document.createElement("p");
+  const $title = document.createElement("span");
+  const $description = document.createElement("span");
+  const $status = document.createElement("span");
+
+  $title.innerText = tasks.title;
+  $description.innerText = tasks.description;
+  $status.textContent = tasks.isComplete ? "Completado" : "Pendiente";
+
+  $info.appendChild($title);
+  $info.appendChild($description);
+  $info.appendChild($status);
+  $container.appendChild($info);
+
+  return $container;
+};
+
 document.addEventListener("DOMContentLoaded", async () => {
   const renderAllTask = async () => {
     $tasksList.innerHTML = "";
@@ -11,35 +32,8 @@ document.addEventListener("DOMContentLoaded", async () => {
       .then((e) => e.json())
       .then((listTasks) => {
         listTasks.forEach((task) => {
-          $tasksList.innerHTML += `
-        <p>
-          <span>
-           <b> ${task.title} <b/>
-          </span>
-          <span>
-            <b>${task.description}</b>
-          </span>
-          <span>
-            <b>${task.isComplete}</b>
-          </span>
-          <div id="botonEliminar">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-              />
-            </svg>
-          </div>
-        </p>
-      `;
+          const statusTasks = task.isComplete ? "Completado" : "Pendiente";
+          $tasksList.appendChild(renderTasks(task));
         });
       });
   };
